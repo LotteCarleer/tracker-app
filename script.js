e8207b2e9b504694d0fd5735a809a28699fdd7eb
Lotte Carleer <lottecarleer@LaptopvanLotte.lan>
2025-03-25 20:12:52 +0100

    storage

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const enter = document.querySelector(".enter");
-enter.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log(e);
+document.querySelector(".enter").addEventListener("click", (e) => {
+  const time = document.getElementById("enter").value;
+  if (time !== "" && !isNaN(time) && Number(time) >= 0) {
+    localStorage.setItem("practiceTime", time);
+    alert("Practice time saved: " + time + " minutes");
+  } else {
+    alert("Please enter a valid time.");
+  }
+});
+
+document.getElementById("enter").addEventListener("input", function () {
+  this.value = this.value.replace(/[^0-9]/g, "");
 });
