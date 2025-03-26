e850826cbf6f16622f2edf8bd3ee9ae5b06cfb59
Lotte Carleer <lottecarleer@laptopinge.guests.thomasmore.be>
2025-03-26 11:56:47 +0100

    feat: html & css aangepast

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ document.querySelector(".enter").addEventListener("click", (e) => {
   const time = document.getElementById("enter").value;
   if (time !== "" && !isNaN(time) && Number(time) >= 0) {
     localStorage.setItem("practiceTime", time);
-    alert("Practice time saved: " + time + " minutes");
-  } else {
-    alert("Please enter a valid time.");
   }
 });
 
