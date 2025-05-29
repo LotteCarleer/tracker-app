8fb2d31f468272f25a9b63ddf2835a1ad9c97ca7
Lotte Carleer <lottecarleer@Laptop-van-Lotte.local>
2025-05-30 00:17:58 +0200

    feat: Tijd verschijnt per dag op homepagina en datum is up to date

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
-document.querySelector(".enter").addEventListener("click", (e) => {
-  const time = document.getElementById("enter").value;
-  if (time !== "" && !isNaN(time) && Number(time) >= 0) {
-    let timesArray = JSON.parse(localStorage.getItem("practiceTimes")) || [];
-    timesArray.push(time);
-    localStorage.setItem("practiceTimes", JSON.stringify(timesArray));
-    document.getElementById("enter").value = ""; // Maak het invoerveld leeg na het indienen
-  }
-});
+window.addEventListener("DOMContentLoaded", () => {
+  const submitBtn = document.querySelector(".enter");
+  const inputField = document.getElementById("enter");
 
-document.getElementById("enter").addEventListener("input", function () {
-  this.value = this.value.replace(/[^0-9]/g, "");
+  // Check of we op log.html zijn (daar bestaat .enter wel)
+  if (submitBtn && inputField) {
+    submitBtn.addEventListener("click", () => {
+      const time = inputField.value;
+      if (time !== "" && !isNaN(time) && Number(time) >= 0) {
+        let timesArray =
+          JSON.parse(localStorage.getItem("practiceTimes")) || [];
+        timesArray.push(time);
+        localStorage.setItem("practiceTimes", JSON.stringify(timesArray));
+        inputField.value = ""; // Maak het invoerveld leeg na het indienen
+      }
+    });
+
+    inputField.addEventListener("input", function () {
+      this.value = this.value.replace(/[^0-9]/g, "");
+    });
+  }
+
+  // Code voor index.html
+  const todayBox = document.querySelector(".vandaag .time");
+  const monthBox = document.querySelector(".month .time");
+
+  if (todayBox && monthBox) {
+    const timesArray = JSON.parse(localStorage.getItem("practiceTimes")) || [];
+    const todayTime = timesArray[timesArray.length - 1] || 0;
+    todayBox.textContent = `${todayTime} min`;
+
+    const monthTotal = timesArray.reduce(
+      (total, current) => total + Number(current),
+      0
+    );
+    monthBox.textContent = `${monthTotal} min`;
+  }
 });
